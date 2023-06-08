6000d977a4bc386f4d530bdb50cebb3120c4499f
ion <leuionofficial@gmail.com>
2023-06-08 12:13:14 +0300

    added new functionality: add, delete, update, UI

--- 03-ts-react-todo-app/todo-app/src/components/ListGroup.tsx
+++ 03-ts-react-todo-app/todo-app/src/components/ListGroup.tsx
@@ -7,12 +7,16 @@ function ListGroup() {
         title: '',
         content: ''
     })
+    const [filterValue, setFilterValue] = useState({
+        title: '',
+        content: ''
+    })
 
-    const handleTitleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleTitleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setInputValue(prev => ({...prev, title: event.target.value}))
     }
 
-    const handleValueInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleValueInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setInputValue(prev => ({...prev, content: event.target.value}))
     }
 
@@ -23,38 +27,71 @@ function ListGroup() {
             content: inputValue.content
         }
             setTodoList((prevList) => [...prevList, newTodo])
-            setInputValue({title: '', content: ''})
+            setInputValue(() => ({content: '', title: ''}))
         }
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
+            handleAddTodo()
+        }
+    }
+
+    const handleFilterInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setFilterValue({title: event.target.value, content: event.target.value})
     }
 
 
     return (
         <div className='container'>
             <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Task title</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1"
-                       placeholder="To do" onChange={handleTitleInput} />
+                <label className="form-label">Task title</label>
+                <input type="text" className="form-control"
+                       placeholder="To do" onChange={handleTitleInput} value={inputValue.title} onKeyUp={handleKeyDown} />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">Task content</label>
-                <input className="form-control" onChange={handleValueInput} />
+                <label className="form-label">Task content</label>
+                <input className="form-control" type='text' value={inputValue.content} onChange={handleValueInput} onKeyUp={handleKeyDown} />
             </div>
             <div>
                 <button className='btn btn-primary' onClick={handleAddTodo}>Submit</button>
             </div>
             <br/>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Filter tasks</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1"
+                       placeholder="Filter" onChange={handleFilterInput}/>
+            </div>
+            <br/>
+            <h3>Task list</h3>
             {todoList.length === 0 && <p>no item found</p>}
             <ul className="list-group">
-                {todoList.map((obj, index: number) => {
+                {todoList.filter((item) => {
+                    return (
+                        item.content.toLowerCase().includes(filterValue.content.toLowerCase())
+                        ||
+                        item.title.toLowerCase().includes(filterValue.title.toLowerCase())
+                    )
+                }).map((obj, index) => {
                   return (
                     <>
-                        <li key={index} className="list-group-item">
-                            <strong>{obj.title}</strong> <br/>
-                            {obj.content}
+                        <li key={index} className="list-group-item d-flex justify-content-between">
+                            <div>
+                                <strong>{obj.title}</strong> <br/>
+                                {obj.content}
+                            </div>
+                            <div>
+                                <button className='btn btn-primary m-2'>Edit</button>
+                                <button className='btn btn-danger'>Delete</button>
+                            </div>
                         </li>
                     </>
                   )
                 })}
+                <div>
+                    <button className='btn btn-danger mt-3'>Delete all</button>
+                </div>
             </ul>
         </div>
     )
